392f9f066206f4a82690e66b44de819497593749
David Adler <dalberto.adler@gmail.com>
2020-01-24 17:16:36 +0000

    add comments

--- main.ts
+++ main.ts
@@ -90,29 +90,36 @@ class DrawTree {
   ): { rightMost: number; left: number } {
     let leftMostChild = Number.POSITIVE_INFINITY
     let rightMostChild = Number.NEGATIVE_INFINITY
+
+    // Draw the subtrees of this node
     for (let i = 0; i < node.children.length; i++) {
       const child = node.children[i]
       if (!child) continue
-      let {
-        left,
-        rightMost: rightMostOfSubtree,
-      } = this.davidCalculatePositions(
+      let subtree = this.davidCalculatePositions(
         child,
         depth + 1,
         siblingCount,
         rightMost,
       )
-      rightMost = Math.max(rightMostOfSubtree, rightMost)
-      leftMostChild = Math.min(leftMostChild, left)
-      rightMostChild = Math.max(rightMostChild, left)
+      // Keep track of the right most contour for each subtree
+      // Ensure the subtrees don't overlap
+      rightMost = Math.max(subtree.rightMost, rightMost)
+
+      // Keep track of extremes of locations of direct children
+      leftMostChild = Math.min(leftMostChild, subtree.left)
+      rightMostChild = Math.max(rightMostChild, subtree.left)
     }
+
     node.position.top = depth
+
     const count = siblingCount.get(depth)
     if (node.children.length >= 1) {
+      // center the parent
       const centered =
         leftMostChild + Math.abs(rightMostChild - leftMostChild) / 2
       node.position.left = centered
     } else {
+      // Don't overlap with this row or left subtree
       node.position.left = Math.max(count, rightMost) + 1
       rightMost++
     }
